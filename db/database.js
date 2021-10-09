6d05590004f02773301d14164c1c0ac2e9c404af
Andrés G <andres88884@hotmail.com>
2021-10-09 15:16:37 -0500

    first commit

---
+++ db/database.js
@@ -0,0 +1,46 @@
+db_periodos = [
+    {
+        "periodo": "2021-09-29",
+        "dias_calendario_periodo_fecha_cobro": "30"
+    },
+    {
+        "periodo": "2021-10-28",
+        "dias_calendario_periodo_fecha_cobro": "28"
+    },
+    {
+        "periodo": "2021-11-29",
+        "dias_calendario_periodo_fecha_cobro": "32"
+    },
+    {
+        "periodo": "2021-12-30",
+        "dias_calendario_periodo_fecha_cobro": "31"
+    },
+    {
+        "periodo": "2022-01-28",
+        "dias_calendario_periodo_fecha_cobro": "29"
+    },
+    {
+        "periodo": "2022-02-25",
+        "dias_calendario_periodo_fecha_cobro": "28"
+    },
+    {
+        "periodo": "2022-03-30",
+        "dias_calendario_periodo_fecha_cobro": "33"
+    },
+    {
+        "periodo": "2022-04-28",
+        "dias_calendario_periodo_fecha_cobro": "29"
+    },
+    {
+        "periodo": "2022-05-30",
+        "dias_calendario_periodo_fecha_cobro": "32"
+    },
+    {
+        "periodo": "2022-06-29",
+        "dias_calendario_periodo_fecha_cobro": "30"
+    },
+    {
+        "periodo": "2022-07-28",
+        "dias_calendario_periodo_fecha_cobro": "29"
+    }
+]
---
+++ js/calc_rappicard.js
@@ -0,0 +1,362 @@
+// TODO - $(document).ready function
+$(document).ready( function() {
+    let fecha_hoy = new Date().toISOString().slice(0, 10);
+    document.getElementById("txtFecha").value = fecha_hoy;
+
+    cargarRangoFechas();
+
+    document.getElementById("cuotaPromedio").innerHTML = '0.00';
+    document.getElementById("totalPagado").innerHTML = '0.00';
+    document.getElementById("interesesPagados").innerHTML = '0.00';
+
+    let llenar_tabla = '';
+    llenar_tabla +=`
+    <tr>
+        <th scope="row">0</th>
+        <td>0000-00-00</td>
+        <td>0.00</td>
+        <td>0.00</td>
+        <td>0.00</td>
+        <td>0.00</td>
+        <td>0.00</td>
+    </tr>
+    `;
+
+    document.getElementById("tabla_cuotas").innerHTML=llenar_tabla;
+
+});
+
+// TODO - function cargarRangoFechas
+function cargarRangoFechas(){
+
+    // primera fecha del array
+    let firstItemArray = db_periodos[0].periodo;
+
+    // ultima fecha del array
+    let lastItemArray = db_periodos[db_periodos.length-1].periodo;
+    
+    //console.log(firstItemArray, ' ', lastItemArray);
+
+    /* https://stackoverflow.com/questions/13571700/get-first-and-last-date-of-current-month-with-javascript-or-jquery */
+    let date = new Date(firstItemArray);
+
+    let firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    let primerDiaMes = firstDay.toISOString().slice(0, 10);
+
+    //var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    //let ultimoDiaMes = lastDay.toISOString().slice(0, 10);
+
+    /* https://stackoverflow.com/questions/32378590/set-date-input-fields-max-date-to-today */
+    document.getElementById("txtFecha").setAttribute("min", primerDiaMes);
+    document.getElementById("txtFecha").setAttribute("max", lastItemArray);
+
+}
+
+// TODO - function validarFecha
+function validarFecha(){
+
+    let fecha = document.getElementById("txtFecha").value;
+    //console.log('validarFecha ',fecha.slice(0, 10));
+
+    // primera fecha del array
+    let firstItemArray = db_periodos[0].periodo;
+    
+    // ultima fecha del array
+    let lastItemArray = db_periodos[db_periodos.length-1].periodo;
+    
+    //console.log(firstItem, ' ', lastItem);
+
+    /* https://stackoverflow.com/questions/13571700/get-first-and-last-date-of-current-month-with-javascript-or-jquery */
+    let date = new Date(firstItemArray);
+    
+    let firstDay = new Date(date.getFullYear(), date.getMonth(), 1);
+    let primerDiaMes = firstDay.toISOString().slice(0, 10);
+    
+    //var lastDay = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+    //let ultimoDiaMes = lastDay.toISOString().slice(0, 10);
+
+    // Si la fecha ingresada por el usuario es menor a la fecha minima permitida, se retorna la primer fecha permitida
+    if(fecha < primerDiaMes){
+        document.getElementById("txtFecha").value = primerDiaMes;
+        return primerDiaMes;
+    // Si la fecha ingresada por el usuario está dentro del rango permitido, se retorna la fecha tal cual
+    }else if(fecha >= primerDiaMes && fecha <= lastItemArray ){
+        document.getElementById("txtFecha").value = fecha;
+        return fecha;
+    // Si la fecha ingresada por el usuario es mayor a la fecha máxima permitida, se retorna la ultima fecha permitida
+    }else{
+        document.getElementById("txtFecha").value = lastItemArray;
+        return lastItemArray;
+    }
+
+}
+
+// TODO - PRINCIPAL function calculadora_rappicard
+function calculadora_rappicard(){
+
+    let compra = parseFloat( document.getElementById("txtCompra").value );
+    let interes_mensual = parseFloat( document.getElementById("txtTasaIntCte").value );
+    let cuotas = parseInt( document.getElementById("txtCuotas").value );
+
+    //var fecha = document.getElementById("txtFecha").value;
+    let fecha = validarFecha();
+    //console.log('fecha_calc_rappi ',fecha);
+
+    let resultado = 0.0;
+
+    if( !isNaN(compra) && !isNaN(interes_mensual) && !isNaN(cuotas) ){
+
+        let llenar_tabla = "";
+
+        //var intereses = 0;
+
+        //var cuota_promedio = 0.0;
+        //var total_pagado = 0.0;
+        let intereses_pagados = 0.0;
+        
+        //var deuda_total = 0.0;
+        //var deuda_total_aux = 0.0;
+
+        let suma_total_cuotas = 0.0;
+
+        // Validación imput txtCuotas para no permitir valores menores a 1
+        if (cuotas < 1) {
+            document.getElementById("txtCuotas").value = 1;
+            cuotas = 1;
+        }
+
+        /* TODO --- Inicio - primera cuota --------------- */
+
+        // CAPITAL
+        //cuota_a_devolver = 83333.33;
+        cuota_a_devolver = (compra/cuotas);
+
+        // SALDO FINAL
+        //deuda_total = 416666.67;
+        //deuda_total_fix = 416666.67;
+        deuda_total = compra - cuota_a_devolver;
+
+        if(deuda_total < 0){
+            deuda_total_fix = 0.00;
+        }else{
+            deuda_total_fix = deuda_total.toFixed(2);
+        }
+
+        //intereses = 643.33;
+        intereses = (compra*interes_mensual)/100;
+
+
+        let contpos = 0;
+        for (let el of db_periodos) {
+            //console.log(el);
+            //var fecha_cobro_mes_1 = el;
+
+            // Se extrae el mes y año de la fecha ingresada por el usuario
+            let mesAnioFecha = fecha.slice(0, 7);
+            //console.log('mes_fecha ',mesAnioFecha);
+
+            let mesAnioPeriodo = el.periodo.slice(0, 7);
+            //console.log('mes_periodo ',mesAnioPeriodo);
+            
+            if ( mesAnioFecha === mesAnioPeriodo ) {
+                
+                // Si la fecha elegida por el usuario es mayor a la fecha de cobro actual, se mueve al siguiente periodo de facturación
+                if(fecha > el.periodo){ 
+
+                    //console.log('true');
+                    contpos = contpos + 1;
+
+                    //console.log(db_periodos[contpos].periodo);
+                    var fecha_cobro_mes_1 = db_periodos[contpos].periodo; // Se mueve al siguiente periodo de facturación y se retorna el dato en fecha_cobro_mes_1
+                    
+                    //9650/30 = 321.66
+                    interes_diario_mes_1 = intereses / el.dias_calendario_periodo_fecha_cobro;
+                    //console.log(interes_diario_mes_1);
+
+                    break;
+                }else{ // Se conserva la fecha de facturación actual
+                    //console.log('false');
+                    var fecha_cobro_mes_1 = el.periodo;
+
+                    //9650/30 = 321.66
+                    interes_diario_mes_1 = intereses / el.dias_calendario_periodo_fecha_cobro;
+                    //console.log(interes_diario_mes_1);
+
+                    break;
+                }
+
+            }
+            //console.log(contpos);
+            contpos = contpos + 1;
+        }
+
+        //console.log('fecha cobro mes 1 ',fecha_cobro_mes_1);
+        //console.log('interes diario mes 1 ',interes_diario_mes_1);
+        //console.log('cuota 1 posicion registro ',contpos);
+
+        let diff = diffFechas(fecha,fecha_cobro_mes_1);
+        //console.log(diff);
+
+        //fecha = '2021-10-05';
+
+        // 321.66 * 2 = 643.33  (2 dias antes del corte de la tarjeta)
+        //dias_antes_del_corte = 2;
+        dias_antes_del_corte = diff; //diferencia de dias antes del corte
+        prorateo_interes_mes_1 = interes_diario_mes_1 * dias_antes_del_corte;
+
+        // CUOTA
+        cuota = cuota_a_devolver + prorateo_interes_mes_1;
+        //console.log(cuota);
+
+        llenar_tabla +=`
+
+        <tr>
+            <th scope="row">1</th>
+            <td>`+fecha_cobro_mes_1+`</td>
+            <td>`+compra.toFixed(2)+`</td>
+            
+            <td>`+cuota_a_devolver.toFixed(2)+`</td>
+            <td>`+prorateo_interes_mes_1.toFixed(2)+`</td>
+            <td>`+cuota.toFixed(2)+`</td>
+            <td>`+ deuda_total_fix +`</td>
+        </tr>
+
+        `;
+
+        //contpos_aux = contpos;
+        //deuda_total_aux = deuda_total;
+
+        /* TODO --- Fin - primera cuota ----------------- */
+
+        /* TODO --- Inicio - calculos cuota 2 hascia adelante y preparación llenado de tabla ----------------- */
+
+        //console.log('contpos_aux antes del for: ', contpos_aux);
+        //console.log('tamaño del array: ', db_periodos.length);
+        //console.log('ultima posicion del array: ', db_periodos.length-1);
+        //console.log('ultimo registro array: ', db_periodos[10]);
+
+
+        /*
+        Hay que validar el tamaño del array de database.js, para saber cuantas cuotas máximo
+        puedo elegir, para no salirme de los periodos disponibles en database.js, 
+        según la fecha escogida en el calendario de la interfaz
+        */
+
+        let tamano_array = db_periodos.length;
+        let posiciones_disponibles_array = contpos;
+
+        let me_puedo_mover = tamano_array - posiciones_disponibles_array;
+        //console.log('me puedo mover por el array: ', me_puedo_mover);
+
+        if (cuotas > me_puedo_mover) {
+            document.getElementById("txtCuotas").value = me_puedo_mover;
+            cuotas = me_puedo_mover;
+        }
+
+
+        for (let i = 1; i < (cuotas); i++) {
+
+            //console.log(db_periodos);
+
+            contpos = contpos + 1;
+            //console.log('cuota ',(i+1),' posicion registro ', contpos, 'periodo ', db_periodos[contpos].periodo);
+
+            //console.log(deuda_total);
+
+            cuota_a_devolver = (compra/cuotas);
+
+            interes_neto = (deuda_total*interes_mensual)/100;
+
+            let month = new Date(db_periodos[contpos].periodo).getMonth() + 1;
+            //let month = db_periodos[contpos].periodo.slice(5, 7);
+            //console.log(month);
+            let year = new Date(db_periodos[contpos].periodo).getFullYear();
+            //let year = db_periodos[contpos].periodo.slice(0, 7);
+            //console.log(year);
+            
+            // https://stackoverflow.com/questions/38672087/get-no-of-days-in-current-month-in-javascript
+            let days_in_month = new Date(year, month, 0).getDate(); // 0 + number of days
+            //console.log(db_periodos[contpos_aux].periodo, ' ', days_in_month);
+
+            //interes_diario = interes_neto / days_in_month; // dias del mes
+            interes_diario = interes_neto / days_in_month; // dias del mes
+            //console.log(interes_diario);
+
+            //intereses
+            intereses = interes_diario * db_periodos[contpos].dias_calendario_periodo_fecha_cobro; // dias de diferencia con el mes anterior - diff
+            
+            //cuota = (compra/cuotas)+intereses;
+            cuota = cuota_a_devolver + intereses;
+            
+            //deuda_total = deuda_total_aux-(cuota_a_devolver-intereses);
+            // SALDO FINAL
+            //deuda_total = 416666.67;
+            //deuda_total_fix = 416666.67;
+            deuda_total = deuda_total - cuota_a_devolver;
+
+            //console.log(cuota_a_devolver);
+
+            if(deuda_total < 0){
+                deuda_total_fix = 0.00;
+            }else{
+                deuda_total_fix = deuda_total.toFixed(2);
+            }
+
+            llenar_tabla +=`
+
+            <tr>
+                <th scope="row">`+(i+1)+`</th>
+                <td>`+db_periodos[contpos].periodo+`</td>
+                <td>`+deuda_total.toFixed(2)+`</td>
+                <td>`+cuota_a_devolver.toFixed(2)+`</td>
+                <td>`+intereses.toFixed(2).toLocaleString( 'es-CO' )+`</td>
+                <td>`+cuota.toFixed(2)+`</td>
+                <td>`+deuda_total_fix+`</td>
+            </tr>
+
+            `;
+
+            //deuda_total_aux = deuda_total;
+
+            intereses_pagados = intereses_pagados + intereses;
+            suma_total_cuotas = suma_total_cuotas + cuota;
+
+        }
+
+        cuota_promedio = suma_total_cuotas/cuotas;
+        total_pagado = compra + intereses_pagados;
+
+        if(isNaN(cuota_promedio)){
+            cuota_promedio = 0;
+        }
+
+        /* TODO --- Fin - calculos cuota 2 hacia adelante y preparación llenado de tabla ----------------- */
+
+        //intereses_pagados = total_pagado-compra;
+
+        document.getElementById("cuotaPromedio").innerHTML=cuota_promedio.toFixed(2);
+        document.getElementById("totalPagado").innerHTML=total_pagado.toFixed(2);
+        document.getElementById("interesesPagados").innerHTML=intereses_pagados.toFixed(2);
+
+        document.getElementById("tabla_cuotas").innerHTML=llenar_tabla;
+
+    }else{
+        document.getElementById("cuotaPromedio").innerHTML="0.0";
+        document.getElementById("totalPagado").innerHTML="0.0";
+        document.getElementById("interesesPagados").innerHTML="0.0";
+    }
+}
+
+// TODO - function diffFechas
+function diffFechas(fechaInicio, fechaFin){
+
+    let dt1 = new Date(fechaInicio);
+    let dt2 = new Date(fechaFin);
+    
+    let result = Math.floor((Date.UTC(dt2.getFullYear(), dt2.getMonth(), dt2.getDate()) - Date.UTC(dt1.getFullYear(), dt1.getMonth(), dt1.getDate()) ) /(1000 * 60 * 60 * 24));
+
+    //console.log(result);
+    return result;
+
+}
+
